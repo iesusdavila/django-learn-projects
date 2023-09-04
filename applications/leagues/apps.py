9463b0574c95a650c1058bc0526494111d3f7373
iesusdavila <iesusdavila@gmail.com>
2023-09-04 18:12:24 -0500

    config applications

--- applications/leagues/apps.py
+++ applications/leagues/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LeaguesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'leagues'
+    name = 'applications.leagues'
--- fifa/settings.py
+++ fifa/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'applications.nations',
+    'applications.players',
+    'applications.teams',
+    'applications.leagues',
 ]
 
 MIDDLEWARE = [
